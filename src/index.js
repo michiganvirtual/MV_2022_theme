d368d6449fe65fbfcdd6c6a40875b351bc27e7f8
robroplol <robrop92@gmail.com>
2023-03-20 14:51:15 -0400

    preflight init

--- src/index.js
+++ src/index.js
@@ -27,6 +27,9 @@ $(document).ready(function () {
     }
   }
 
+  //Add aria-hidden=true to all H1 tags
+  document.querySelectorAll("h1")[0].setAttribute("aria-hidden", true);
+
   $(".flip-card").attr("tabindex", "0");
   $(".flip-card").keypress(function (e) {
     e.preventDefault();
