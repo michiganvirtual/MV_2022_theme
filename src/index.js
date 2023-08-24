30d984fb48a445baceae4ed0aaa7d3b06a6ed531
robroplol <robrop92@gmail.com>
2023-08-24 09:26:15 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -539,9 +539,6 @@ $(window).load(function () {
     $("#respond-btn").addClass("hidden");
     $(".response-container").selectable("disable");
   });
-
-  //Body Overflow Reset
-  $("body").style.overflow = "auto";
 });
 
 //Add keyboard navigation functionality
