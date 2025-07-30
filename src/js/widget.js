e8f440e60f13999a0ba47d7cae7f312dd543c3f9
robroplol <robrop92@gmail.com>
2025-07-30 16:15:52 -0400

    font issues

--- src/js/widget.js
+++ src/js/widget.js
@@ -9,7 +9,7 @@ class HelpWidget extends HTMLElement {
 
     container.innerHTML = `
         <style>
-        /* @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');*/
+        /* @import url('https://mv-2022-theme.netlify.app/output.css');*/
                 
         @font-face {
           font-family: "FontAwesome";
